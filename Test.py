c174ddaab4597fa2990d9ba8818e2cc806463d52
KolaSamuel <thekolasamuel@gmail.com>
2018-04-19 11:27:46 +0100

    Updated Shape class

--- Test.py
+++ Test.py
@@ -1,23 +1,18 @@
 from utils import SolutionGraph
 from collections import Counter
 from timeit import timeit
+from geometry import *
 import numpy as np
 
 A1 = np.array(range(200))
 A = list(range(200))
 
 B = ['area = pi*radius**2', 'diameter = 2*radius', 'perimeter = 2*pi*radius', 'thy = trt = 3']
-tree = SolutionGraph(B)
-print(tree)
+data = {"radius": 3}
+find = {"area"}
+question = Circle(data, find)
+print(question["area"])
+print(question["radius"])
+print(question["theta"])
 
-
-# def tester():
-#     equations(['area = pi*radius**2',
-#                'diameter = 2*radius',
-#                'perimeter = 2*pi*r',
-#                ], A)
-#
-#
-# print('functions: ', timeit("1*2", setup="from __main__ import tester, A, A1"))
-# print('ranges: ', timeit("A1*2", setup="from __main__ import tester, A, A1"))
 
--- utils.py
+++ utils.py
@@ -4,21 +4,20 @@ Various tools and utilities used
 from sympy import solve, sympify, Eq, symbols
 from collections import defaultdict
 from datascience import Table
+import sys
+
+sys.setrecursionlimit(1000)
 
 
-class Node(Table):
+class SGNode(Table):
     """
-        pre- initialized table+ class
+        Solution Graph Node(SGNode), pre- initialized table+ class
     """
     def __init__(self):
         labels = ['Equation', 'Symbols',
                   'Complexity']
         Table.__init__(self, labels=labels)
         self.all_children = set()
-
-    def update(self, row, children):
-        self.append(row)
-        self.all_children.update(children)
 
 
 class SolutionGraph(object):
@@ -27,18 +26,22 @@ class SolutionGraph(object):
         to convert problem into search problem.
     """
 
-    def __init__(self, equations, data):
+    def __init__(self, equations, data, find):
         """
-            Initializes and builds solution graph, stores data(changing the
-            data's "key identifiers" from string to symbols) and creates a set of
-            all data already found or given.
+            Initializes and builds solution graph, stores data( also changes the
+            data's "keys" from string to symbols)
         :param equations: array of string expressions
         :param data: already given data
+        :param find: list of query's
         """
         # Initializations
-        self.tree = defaultdict(Node)
+        self.tree = defaultdict(SGNode)
         self.data = {symbols(variable): data[variable] for variable in data}
         self.data_set = set(self.data)
+        # Special tree Heuristic
+        # Uses query list and data list to determine best path
+        to_find = set([symbols(variable) for variable in find])
+        self.all_data_set = self.data_set.union(to_find)
 
         # Build graph
         for equation in equations:
@@ -62,16 +65,59 @@ class SolutionGraph(object):
                 new_formula = solve(formula, variable)[-1]
 
                 # new_variables == no of variables in formula that are unknown
-                new_variables = new_formula.free_symbols - self.data_set
-                row = [new_formula, new_variables, len(new_variables)]
+                new_variables = new_formula.free_symbols
+                heuristic = len(new_variables-self.all_data_set)
 
-                # updates
-                self.tree[variable].update(row, new_variables)
+                # **********Untested pruning Idea**********
+                # if heuristic == len(new_variables):
+                #     continue
+                # *****************************************
+
+                row = [new_formula, new_variables, heuristic]
+
+                # updates on node
+                self.tree[variable].append(row)
 
         except ValueError:
             print('Equation Error: Invalid expression ',
                   equation, '\n \t Ignoring expression...\n')
             pass
+
+    def data_update(self, node, row):
+        # calculate dependent variable given complete set of
+        # independent variables in self.data
+        self.data[node] = row.item("Equation").evalf(subs=self.data)
+        self.data_set.add(node)
+        self.all_data_set.add(node)
+
+    def search(self, node, visited):
+        can_solve = False  # unnecessary initialization. Has no effect
+        row_len = self.tree[node].num_rows
+        for index in range(row_len):
+            row = self.tree[node].row(index)
+
+            # can be made faster somehow
+            children = row.item("Symbols")
+            can_solve = children.issubset(self.data_set)
+
+            # update
+            if can_solve:
+                self.data_update(node, row)
+                return can_solve
+
+            for child in children:
+                path = (node, child)
+                if path in visited:
+                    continue
+                visited.add(path)
+                can_solve = self.search(child, visited)
+
+                # update
+                if can_solve:
+                    self.data_update(node, row)
+                    return can_solve
+
+        return can_solve
 
     def __getitem__(self, item):
         """
@@ -82,10 +128,19 @@ class SolutionGraph(object):
         query = symbols(item)
 
         if query in self.data_set:
-            print('Redundant query, value already previously found as ',
-                  self.data[query])
+            print('Redundant query,', query, 'already previously found as :', )
+            return self.data[query]
 
+        start_node = query
         visited = set()
+
+        solution = self.search(start_node, visited)
+
+        if solution:
+            print(item.title(), "found! :")
+            return self.data[query]
+        else:
+            insufficient_data_handler(item, self.data)
 
     def __str__(self):
         string = ''
@@ -126,7 +181,7 @@ def insufficient_data_handler(query, data):
             data[variable] = int(value)
             try:
                 variable, value = input("Input: ").strip().split()
-                variable.lower()
+                variable = symbols(variable.lower())
             except ValueError:
                 variable = 'done'
 
