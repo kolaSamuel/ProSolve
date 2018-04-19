30813f8bf0e2fbb50e133dfe11dde820bb59d8ea
KolaSamuel <thekolasamuel@gmail.com>
2018-04-19 11:27:35 +0100

    Updated Shape class

--- geometry.py
+++ geometry.py
@@ -8,16 +8,18 @@ from utils import *
 
 class Circle(object):
 
-    def __init__(self, data):
-
-        self.data = data
+    def __init__(self, data, find):
+        """
+        :param data: list of given data from question
+        :param find: list of query's from question
+        """
         self.formulas = ['area = pi*radius**2',
                          'diameter = 2*radius',
                          'perimeter = 2*pi*radius',
                          ]
 
         # initialises formulas for SolutionGraph search
-        self.find = SolutionGraph(self.formulas, data)
+        self.find = SolutionGraph(self.formulas, data, find)
 
     def __getitem__(self, query):
         """
